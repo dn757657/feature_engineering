cbbe02fa663e8f740cba89ebe9de6c70c8991f86
dn757657 <daniel.js.campbell@gmail.com>
2023-08-30 13:25:35 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open("./requirements.txt") as requirement_file:
 setup(
     name="feature_engineering",
     description="feature engineering and cleaning",
-    version="0.1.13",
+    version="0.1.14",
     author="dn757657",
     author_email="dn757657@dal.ca",
     install_requires=requirements,
     packages=find_packages(include=['dn757657_data_pre_processing',
-                                    'dn757657_feature_selection'],
-                           exclude=['dn757657_features']),  # package = any folder with an __init__.py file
+                                    'dn757657_feature_selection',
+                                    'dn757657_features']),  # package = any folder with an __init__.py file
 )
